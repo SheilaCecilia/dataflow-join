5c046552680f8faa89ccec21b57309a03dbe8b33
SheilaCecilia <djyzhwy@gmail.com>
2020-03-29 20:46:40 +0800

    read plan from file

--- src/wings_plan/plan.rs
+++ src/wings_plan/plan.rs
@@ -6,7 +6,13 @@ use timely::dataflow::operators::*;
 use std::sync::{Arc, Mutex};
 use timely::dataflow::{ProbeHandle};
 use timely::dataflow::operators::{Exchange, Inspect, Probe};
-use timely::{Data, ExchangeData};
+use timely::{ExchangeData};
+
+use std::io::BufReader;
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
 
 use super::Indexable;
 
@@ -16,7 +22,7 @@ use wings_plan::ExtendEdges;
 pub type Node = u32;
 pub type Edge = (Node, Node);
 
-//#[derive(Debug, Default)]
+#[derive(Debug, Default)]
 pub struct PlanNode{
     // edges[edge_start_idx, edge_start_idx + num_edges] are the out edges of this node
     edge_start_idx: usize,
@@ -25,21 +31,21 @@ pub struct PlanNode{
     is_query: bool,
 }
 
-//#[derive(Debug, Default)]
+#[derive(Debug, Default)]
 pub struct PlanOperation{
     src_key: usize,
     dst_key: usize,
     is_forward: bool,
 }
 
-//#[derive(Debug, Default)]
+#[derive(Debug, Default)]
 pub struct PlanEdge{
     src: Rc<PlanNode>,
     dst: Rc<PlanNode>,
     operations: Vec<PlanOperation>,
 }
 
-//#[derive(Debug, Default)]
+#[derive(Debug, Default)]
 pub struct Plan{
     edges: Vec<PlanEdge>,
     nodes: Vec<Rc<PlanNode>>,
@@ -66,22 +72,23 @@ impl Plan{
 
         for index in start_idx .. end_idx{
             let child = self.edges[index].dst.clone();
-            let output:Stream<G, (P, i32)>;
+
             let counter1 = counter.clone();
             let counter2 = counter.clone();
-            if root.subgraph_num_vertices < child.subgraph_num_vertices{
+
+            let output = if root.subgraph_num_vertices < child.subgraph_num_vertices{
                 let attributes = self.edges[index].get_extend_attributes();
-                output = stream.extend_attributes(graph, &attributes)
+                stream.extend_attributes(graph, &attributes)
                     .flat_map(|(p, es, w)|
                         es.into_iter().map(move |e|  {
                             let mut clone = p.clone();
                             clone.push(e);
                             (clone, w)
-                        }));
+                        }))
             }else{
                 let attributes = self.edges[index].get_intersect_attributes();
-                output = stream.intersect_attributes(graph, &attributes);
-            }
+                stream.intersect_attributes(graph, &attributes)
+            };
             if child.is_query{
                 output.probe_with(probe);
                 output.exchange(|x| (x.0).index(0) as u64)
@@ -121,14 +128,81 @@ impl PlanEdge{
             }
         }
 
-        println!("get_intersect_attributes: {:?}", constraints);
-
         constraints
     }
 }
 
+
+pub fn read_plan(filename:&str) -> Plan{
+    let path = Path::new(filename);
+    let display = path.display();
+
+    let file = match File::open(&path){
+        Err(why) => {
+            panic!("EXCEPTION: couldn't open {}: {}",
+                    display,
+                    Error::description(&why))
+        }
+        Ok(file) => file,
+    };
+
+    let mut reader = BufReader::new(file);
+    let mut plan:Plan = Default::default();
+
+    let mut line = String::new();
+    reader.read_line(&mut line).unwrap();
+    let nodes: usize = line.trim().parse().unwrap();
+
+    let mut edge_start_idx = 0;
+
+    for _i in 0 .. nodes {
+        line = String::new();
+        reader.read_line(&mut line).unwrap();
+        let elts: Vec<&str> = line[..].split_whitespace().collect();
+        let num_edges: usize = elts[0].parse().unwrap();
+        let subgraph_num_vertices: usize = elts[1].parse().unwrap();
+        let is_query: bool = elts[2].parse().unwrap();
+        plan.nodes.push(Rc::new(PlanNode{ edge_start_idx, num_edges, subgraph_num_vertices, is_query }));
+        edge_start_idx += num_edges;
+    }
+
+    line = String::new();
+    reader.read_line(&mut line).unwrap();
+    let edges: usize = line.trim().parse().unwrap();
+
+    for _i in 0 .. edges {
+        line = String::new();
+        reader.read_line(&mut line).unwrap();
+        let elts: Vec<&str> = line[..].split_whitespace().collect();
+        let src: usize = elts[0].parse().unwrap();
+        let dst: usize = elts[1].parse().unwrap();
+        let num_operations: usize = elts[2].parse().unwrap();
+
+        let mut operations = vec![];
+        
+        for _j in 0 .. num_operations {
+            line = String::new();
+            reader.read_line(&mut line).unwrap();
+            let elts: Vec<&str> = line[..].split_whitespace().collect();
+            let src_key: usize = elts[0].parse().unwrap();
+            let dst_key: usize = elts[1].parse().unwrap();
+            let is_forward: bool = elts[2].parse().unwrap();
+
+            operations.push(PlanOperation{src_key, dst_key, is_forward});
+        }
+
+        plan.edges.push(PlanEdge{
+            src: plan.nodes[src].clone(),
+            dst: plan.nodes[dst].clone(),
+            operations,
+        })
+    }
+
+    plan
+}
+
 pub fn get_test_extend_plan() -> Plan{
-    let mut plan = Plan{edges: vec![], nodes: vec![], root_node_id: 0};
+    let mut plan:Plan = Default::default();
 
     plan.root_node_id = 0;
 
